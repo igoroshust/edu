9d820b4d08905c8ad0aeb978922afc5b75bfcb4b
igoroshust <igoroshust75@gmail.com>
2024-01-25 12:36:40 +0900

    stash test

--- autotest.py
+++ autotest.py
@@ -10,10 +10,17 @@ driver = webdriver.Chrome()
 driver.get("https://google.com")
 # driver.get("http://130.193.37.179/app/pets")
 # (driver.find_elements(By.XPATH, "//*[@id=\"image\"]/img"))[0].click()
-driver.find_elements(By.XPATH, "//textarea[@title=\"Поиск\"]").send_keys('Skillfactory' + Keys.RETURN) # относится к google
-sleep(2) относится к google
+driver.find_element(By.XPATH, "//textarea[@title=\"Поиск\"]").send_keys('Skillfactory' + Keys.RETURN) # относится к google
+sleep(2) # относится к google
 driver.save_screenshot('sf.png') # относится к google
 driver.quit()
+
+sleep(3)
+print(4)
+print(5)
+print(6)
+a = [i**2 for i in range(10)]
+print(a)
 
 
 # //*[@id="APjFqb"]
